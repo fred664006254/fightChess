89cc16d9a406de4663e330235ceea74c36f60bdb
fred664006254 <fred664006254@163.com>
2019-04-28 17:31:38 +0800

    开发

--- FightChess/src/Message/LocMessage.ts
+++ FightChess/src/Message/LocMessage.ts
@@ -2,5 +2,6 @@ module Jun {
     export const NOTICE = {
         STARTUP : `startup`,//启动MVC
         CLIENT_INITED : `client_inited`,//客户端初始化完成
+        CHECK_INIT : "check_init",//检查游戏初始化完成，需要资源及用户数据都获取到
     }
 }
--- FightChess/src/Mvc/App/ApplicationMediator.ts
+++ FightChess/src/Mvc/App/ApplicationMediator.ts
@@ -15,7 +15,7 @@ module Jun {
 
         public listNotificationInterests(): Array<any> {
             return [
-                
+                NOTICE.CHECK_INIT,//检查游戏初始化完成，需要资源及用户数据都获取到
             ];
         }
 
@@ -24,8 +24,57 @@ module Jun {
             let main = <AppContainer><any>(this.viewComponent);
 
             switch (notification.getName()) {
+                case NOTICE.CHECK_INIT:
+                    this.check_enter_game();
+                    break;
                 default:
                     break;
+            }
+        }
+
+        private check_enter_game(): void {
+            if (!Lang || !MX_SERD.server_list.length) {//等待初始化完成
+                return;
+            }
+
+            if (Tools.can_local_s) {//可以存储
+                let cstate: any = egret.localStorage.getItem(MX_COMMON.MX_MLS_KEY);//背景音乐
+                if (cstate) {
+                    if (cstate == "close") {
+                        MX_COMMON.MUSIC_PLAY = false;
+                    }
+                } else {
+                    egret.localStorage.setItem(MX_COMMON.MX_MLS_KEY, "open");
+                }
+                cstate = egret.localStorage.getItem(MX_COMMON.MX_MELS_KEY);//音效
+                if (cstate) {
+                    if (cstate == "close") {
+                        MX_COMMON.MUSIC_EFFECT_PLAY = false;
+                    }
+                } else {
+                    egret.localStorage.setItem(MX_COMMON.MX_MELS_KEY, "open");
+                }
+            } else {
+                MX_COMMON.MUSIC_PLAY = false;//默认关闭背景音乐
+                MX_COMMON.MUSIC_EFFECT_PLAY = false;//默认关闭音效
+                this.sendNotification(MX_NOTICE.SHOW_TOP_UI, { "text": Lang.p0100 });
+            }
+
+            let mxr = mx_get_opt("mxr");
+            if (mxr) {//已经校验过了，直接进入游戏
+                this.check_enter_screen();
+                return;
+            }
+
+            let share_sid = mx_get_opt("mx_share_sid");//分享者sid
+            if (share_sid) {//从分享链接进入
+                if (AppConfig.MXTag.indexOf("h5") == -1) {//非H5渠道，不需要校验平台
+                    this.check_enter_screen();
+                } else {
+                    this.check_plat();//校验分享
+                }
+            } else {
+                this.check_enter_screen();
             }
         }
     }
